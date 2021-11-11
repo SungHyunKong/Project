28196979758ce2831ed6b0cc5c68a99185e7a44f
sunghyunkong <tkrxod@knou.ac.kr>
2021-11-11 12:48:16 +0900

    2021-11-11 12:50

--- Project/src/project/scoremanagement/StuForm.java
+++ Project/src/project/scoremanagement/StuForm.java
@@ -1,8 +1,19 @@
 package project.scoremanagement;
 
-public class StuForm {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class StuForm  extends JFrame {
 	public static void main(String[] args) {
-		System.out.println("test");
+		JFrame frame = new JFrame("己利包府 橇肺弊伐");
+		frame.setLocation(500, 400);
+		frame.setPreferredSize(new Dimension(300, 500));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
 	}
 
 }
