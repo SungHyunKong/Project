94e4f2378ae799a92d4d58e089714550d21a552f
sunghyunkong <tkrxod@knou.ac.kr>
2021-11-11 11:21:06 +0900

    2021-11-11 11:19

--- Project/src/project/scoremanagement/StuForm.java
+++ Project/src/project/scoremanagement/StuForm.java
@@ -3,7 +3,7 @@ package project.scoremanagement;
 public class StuForm {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("12");
+		System.out.println("test");
 	}
 
 }
